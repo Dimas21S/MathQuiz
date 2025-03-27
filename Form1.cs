f01734fd1f8a9ba3eaaf8fbc0ee0e43affeda5e3
Dimas Satyo <dimassatyo40@gmail.com>
2025-03-27 20:03:58 +0700

    Perbaikan kode

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,11 @@ namespace DeQuiz
 {
     public partial class Form1 : Form
     {
+        int nomorQuiz;
         int angkaPertama;
         int angkaKedua;
         int hasil;
+        int jawaban;
         int poin;
         int menit;
         int detik;
@@ -13,13 +15,49 @@ namespace DeQuiz
         public Form1()
         {
             InitializeComponent();
+            random = new Random();
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
             timer1.Enabled = true;
+            nomorQuiz += 1;
+            labelNomorQuiz.Text = nomorQuiz.ToString();
+        }
 
-            random = new Random();
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                detik++;
+
+                if (detik == 60)
+                {
+                    detik = 0;
+                    menit++;
+                }
+            }
+
+            if (menit == 5)
+            {
+                timer1.Enabled = false;
+            }
+
+            labelMinute.Text = menit.ToString("D2");
+            labelSeconds.Text = detik.ToString("D2");
+        }
+
+        private void GenerateQuestion()
+        {
+            if (!timer1.Enabled)
+            {
+                return;
+            }
             angkaPertama = random.Next(1, 15);
             angkaKedua = random.Next(1, 15);
 
@@ -41,39 +79,27 @@ namespace DeQuiz
                     hasil = angkaPertama / angkaKedua;
                     break;
             }
-            ;
 
             labelAngka1.Text = angkaPertama.ToString();
             labelAngka2.Text = angkaKedua.ToString();
             labelOperasi.Text = operation;
-            labelHasilPoint.Text = poin.ToString();
-        }
-
-        private void buttonStop_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = false;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (timer1.Enabled)
-            {
-                detik++;
-
-                if (detik == 60)
-                {
-                    detik = 0;
-                    menit++;
-                }
-            }
-
-            labelMinute.Text = menit.ToString("D2");
-            labelSeconds.Text = detik.ToString("D2");
         }
 
         private void buttonEnter_Click(object sender, EventArgs e)
         {
-           
+            if (int.TryParse(textBoxHasil.Text, out jawaban))
+            {
+                if (jawaban == hasil)
+                {
+                    poin += 10;
+                }
+            }
+
+            nomorQuiz++; 
+            labelNomorQuiz.Text = nomorQuiz.ToString();
+            labelHasilPoint.Text = poin.ToString();
+            textBoxHasil.Clear();
+            GenerateQuestion();
         }
     }
 }
